bebe0f842d158ff8e20b5f35cee4445793f98c3f
zzsdeo <zzsdeo@gmail.com>
2015-09-14 10:55:22 +0300

    change db location to sdcard

--- app/src/main/java/ru/zzsdeo/money/db/DatabaseHelper.java
+++ app/src/main/java/ru/zzsdeo/money/db/DatabaseHelper.java
@@ -3,14 +3,24 @@ package ru.zzsdeo.money.db;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Environment;
+
+import java.io.File;
 
 class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "money.db";
+    private static final String FILE_DIR = "database";
     private static final int DATABASE_VERSION = 1;
 
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        super(context,
+                context.getExternalFilesDir(null)
+                        + File.separator
+                        + FILE_DIR
+                        + File.separator
+                        + DATABASE_NAME,
+                null, DATABASE_VERSION);
     }
 
     // Method is called during creation of the database
