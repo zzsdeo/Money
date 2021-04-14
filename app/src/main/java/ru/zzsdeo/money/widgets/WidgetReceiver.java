9aea761a0162047075227d809afcb211c17275c1
zzsdeo <zzsdeo@gmail.com>
2021-04-14 12:01:34 +0300

    update dependencies, refactoring to support android 11, bump version to 1.3

--- app/src/main/java/ru/zzsdeo/money/widgets/WidgetReceiver.java
+++ app/src/main/java/ru/zzsdeo/money/widgets/WidgetReceiver.java
@@ -18,6 +18,8 @@ public class WidgetReceiver extends AppWidgetProvider {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
 
         Intent intent = new Intent(context, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                Intent.FLAG_ACTIVITY_CLEAR_TASK);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
         for (int appWidgetId : appWidgetIds) {
